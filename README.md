345f7a5ba5c837170e2a3b9745777d1d0bed8dd9
Maialen <34455658+Ubikitina@users.noreply.github.com>
2024-04-20 13:15:02 +0200

    Comments added

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# simple-chat
+# Datahack exercise: AWS Advertisement Website Backend
 
-Simple serverless chat
+This repository contains the project for the "Data Architecture and Engineering" master's program, specifically focusing on the Cloud module. The objective of this exercise is to design and implement the backend of an advertisement website using AWS and Serverless.
+
+## Implemented Requirements
+
+The following requirements have been implemented:
+
+- **Listing Ads**: Implementation of a API endpoint to list all ads registered in the database.
+- **View Ad Details**: Creation of a API endpoint to view the details of a specific ad.
+- **Publishing Ads**: Development of a API endpoint to publish a new text advert.
+- **Chat/Comments Feature**: Integration of a chat/comments system associated with each ad. Source code for the chat: https://github.com/sergiokhayyat/simple-chat/tree/master
+
+## Focus Areas
+
+- **Cost Minimization**: Emphasis on minimizing operating costs, particularly during idle periods or when there are no active users. This includes consideration of both service costs and human resource costs.
+- **Serverless Technologies**: Special attention has been given to leveraging serverless technologies to achieve the project's objectives efficiently.
+
--- handler.py
+++ handler.py
@@ -7,6 +7,8 @@ from datetime import datetime, timezone
 # Instantiate messages table
 messages_table = boto3.resource('dynamodb').Table(os.environ.get('DYNAMODB_MESSAGES_TABLE'))
 
+
+# Define a function named get_messages which takes two arguments: event and context
 def get_messages(event, context):
     """Return the messages published in the chat room
 
@@ -22,32 +24,42 @@ def get_messages(event, context):
             ]
         }
     """
+
+    # Extract the chat_id from the pathParameters in the event object, if not found, set it to None
     chat_id = event.get('pathParameters', {}).get('chat_id')
+
+    # Query the messages_table using the chat_id as a KeyConditionExpression
     messages = messages_table.query(KeyConditionExpression=Key('chat_id').eq(chat_id))
+
+    # Check if the "Items" key exists in the messages dictionary
     if "Items" in messages:
+        # Construct a body dictionary containing status code 200 and a list of messages
         body = {
             "status": 200,
             'messages': [ {'ts': x['ts'], 'user_id': x['user_id'], 'text': x['text']} for x in messages["Items"] ],
         }
+        # Construct a response dictionary with statusCode 200 and the body containing JSON serialized body dictionary
         response = {
             "statusCode": 200,
             "body": json.dumps(body)
         }
-    else:
+    else: # If no items found, construct a body dictionary for 404 error
         # No Item == 404
         body = {
             "status": 404,
             "title": "Chat not found",
             "detail": f"Chat {chat_id} not found in database",
         }
+        # Construct a response dictionary with statusCode 404 and the body containing JSON serialized body dictionary
         response = {
             "statusCode": 404,
             "body": json.dumps(body)
         }
-    # return
+    # Return the constructed response
     return response
 
 
+# Define a function named send_message which takes two arguments: event and context
 def send_message(event, context):
     """Send a message into a chat room
 
@@ -63,8 +75,13 @@ def send_message(event, context):
 
     :rtype: SimpleResponse
     """
+    # Extract the chat_id from the pathParameters in the event object, if not found, set it to None
     chat_id = event.get('pathParameters', {}).get('chat_id')
+
+    # Load the JSON data from the body of the event, if no body present, set it to an empty dictionary
     message = json.loads(event.get('body', '{}'))
+
+    # Put a new item into the messages_table with the provided chat_id, timestamp, user_id, and text
     messages_table.put_item(
         Item={
             'chat_id': chat_id,
@@ -73,11 +90,14 @@ def send_message(event, context):
             'text': message['text'],
         }
     )
+    # Construct a body dictionary indicating a successful creation (status code 201)
     body = {
-        "status": 201,
+        "status": 201, # 201 means the request has succeeded and has led to the creation of a resource
         "title": "OK",
         "detail": f"New message posted into chat {chat_id}",
     }
+
+    # Return a dictionary containing the statusCode 201 and the body containing JSON serialized body dictionary
     return {
         "statusCode": 201,
         "body": json.dumps(body)
